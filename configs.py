1aba8206e0be57d3c9ee1d1e9d4f5bd64dfeebf4
izeynadipathi <101091496+izeynadipathi@users.noreply.github.com>
2022-03-08 01:11:51 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -22,5 +22,32 @@ class Configs:
     UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DoellBarr/solidmusic")
     AUTO_LEAVE = int(getenv("AUTO_LEAVE", "1800"))
 
+# ID dari Channel Atau Group Untuk Wajib Subscribenya
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
+
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+# Pesan Awalan /start
+START_MSG = os.environ.get(
+    "START_MESSAGE",
+    "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>",
+)
+try:
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "").split())]
+except ValueError:
+    raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
+
+# Pesan Saat Memaksa Subscribe
+FORCE_MSG = os.environ.get(
+    "FORCE_SUB_MESSAGE",
+    "<b>Hello {first}\n\nAnda harus bergabung di Channel/Grup saya Terlebih dahulu untuk Melihat File yang saya Bagikan\n\nSilakan Join Ke Channel & Group Terlebih Dahulu</b>",
+)
+
+# Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+# Setel True jika Anda ingin Menonaktifkan tombol Bagikan Kiriman Saluran Anda
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == "True"
 
 config = Configs()
